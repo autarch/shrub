9fe2519be6947fa47c955f29ffed9e877405588c
Dave Rolsky <autarch@urth.org>
2026-05-01 10:54:04 -0500

    DEVPROD-32411 Make `CmdExecShell.AddExpansionsToEnv` a `bool` (#66)

--- operations.go
+++ operations.go
@@ -61,7 +61,7 @@ type CmdExecShell struct {
 	Script                        string            `json:"script" yaml:"script"`
 	Shell                         string            `json:"shell,omitempty" yaml:"shell,omitempty"`
 	Env                           map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
-	AddExpansionsToEnv            map[string]string `json:"add_expansions_to_env,omitempty" yaml:"add_expansions_to_env,omitempty"`
+	AddExpansionsToEnv            bool              `json:"add_expansions_to_env,omitempty" yaml:"add_expansions_to_env,omitempty"`
 	IncludeExpansionsInEnv        []string          `json:"include_expansions_in_env,omitempty" yaml:"include_expansions_inenv,omitempty"`
 	AddToPath                     []string          `json:"add_to_path,omitempty" yaml:"add_to_path,omitempty"`
 	ContinueOnError               bool              `json:"continue_on_err,omitempty" yaml:"continue_on_err,omitempty"`
